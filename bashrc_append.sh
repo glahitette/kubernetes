2c2d5530217fd4aab00e0a3f5acf196a40220df1
Guillaume Lahitette <guillaume@lahitette.net>
2023-02-01 10:41:58 +0100

    Updates

--- bashrc_append.sh
+++ bashrc_append.sh
@@ -4,10 +4,15 @@ echo "alias k=kubectl" >> ~/.bashrc
 echo "alias ka='kubectl apply -f '" >> ~/.bashrc
 echo "alias ka2='f() { [ "$1" ] && kubectl apply -f $1 ; } ; f'" >> ~/.bashrc
 echo "alias kd='k get deployments -o wide'" >> ~/.bashrc
-echo "alias kp='k get pods -o wide --show-labels'" >> ~/.bashrc
+echo "alias kdd='k describe deployment '" >> ~/.bashrc
+echo "alias kdn='k describe node '" >> ~/.bashrc
+echo "alias kdp='k describe pod '" >> ~/.bashrc
+echo "alias kds='k describe service '" >> ~/.bashrc
 echo "alias kno='k get nodes -o wide'" >> ~/.bashrc
 echo "alias kn='kubectl config set-context --current --namespace '" >> ~/.bashrc
 echo "alias kn2='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d " " -f6 ; } ; f'" >> ~/.bashrc
+echo "alias kp='k get pods -o wide --show-labels'" >> ~/.bashrc
+echo "alias ks='k get service -o wide'" >> ~/.bashrc
 echo "source /etc/bash_completion" >> ~/.bashrc
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 echo "complete -F __start_kubectl k" >> ~/.bashrc
