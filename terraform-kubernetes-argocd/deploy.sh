7537781eed386462f2f54aca3a7e849c23765b34
Guillaume Lahitette <guillaume@lahitette.net>
2023-03-03 12:53:39 +0100

    Moved to own project

--- terraform-kubernetes-argocd/deploy.sh
+++
@@ -1,9 +0,0 @@
-terraform init
-
-terraform apply -auto-approve
-
-rm -rf argocd-password.txt
-
-kubectl get secret argocd-initial-admin-secret -n argocd --template={{.data.password}} | base64 -D > argocd-password.txt
-
-kubectl port-forward service/argocd-server 8443:443 -n argocd
